99a46d7506d551ec0e6e6c27a1168198b0308bb2
pleycpl <h4rvey@protonmail.com>
2017-10-16 18:26:43 +0000

    bazi kizimlar duzeltildi.

--- index.js
+++ index.js
@@ -1,71 +1,78 @@
-var request = require("sync-request"),
-    cheerio = require("cheerio"),
-    fs = require('fs'),
-    underscore = require('underscore');
-let  doc = [];
-
+/*
 const similizeSchool = function(data, row) {
   const similizeSchol =  underscore.find(data, function(x) {
     return ((x.district === row.district) && (x.school === row.school));
   });
   return similizeSchol ? true : false;
 };
+*/
 
-var htmlParser = () => {
-  let cityCode = 1;
+const request = require('request')
+const cheerio = require('cheerio')
+const async = require('async')
+const fs = require('fs')
+var SchoolsJsonData = [];
 
-  for (var i = cityCode; i<82; i++) {
-    let pageCount = 1;
-    let hasTd = true;
-    do {
-      let url = "http://www.meb.gov.tr/baglantilar/okullar/?ILKODU="+ i +"&ILCEKODU=&SAYFANO=" + pageCount;
-
-
-      var res = request('GET', url);
-      var body = res.getBody();
-
-      if (body) {
-        var $ = cheerio.load(body, {xmlMode: true})
-        var td = $('td');
-        for (var j = 0; j<td.length; j++) {
-          td.each(function(i, td) {
-            var children = $(this).children();
-            if (children[0].children[0] && children[0].children[0].data) {
-              const data = children[0].children[0].data.toLowerCase();
-              var row = {
-                city : data.split("-")[0],
-                district: data.split("-")[1],
-                school: data.split("-")[2]
-              };
-              if (row.city && row.district && row.school && !similizeSchool(doc, row)) {
-                doc.push(row);
+let main = (callback) => {
+  console.log("[+] Main function!")
+  let url = "", href = "", TotalPageSize = 0
+  for (let CityCode = 1; CityCode < 82; CityCode++) {
+    // Burda Ilkoduna gore istek atiyoruz ve son sayfa numarasini ogreniyoruz.
+    url = `http://www.meb.gov.tr/baglantilar/okullar/?ILKODU=${CityCode}`
+    console.log(url)
+    request(url, (err, resp, html) => {
+      if (err) return null
+      // <a class="last" href="?ILKODU=1&ILCEKODU=&SAYFANO=47" title="Son Sayfa">...</a>
+      const $ = cheerio.load(html, {xmlMode: true})
+      href = $('.last').attr('href')
+      TotalPageSize = href.split("=").pop()
+      if (TotalPageSize === 0 || TotalPageSize === undefined) return null
+      console.log(TotalPageSize)
+      // Sayfa sayisini bulduktan sonra, tek tek sayfalardaki data yi aliyoruz.
+      for(let PageNumber = 1; PageNumber <= TotalPageSize; PageNumber++) {
+        url = `http://www.meb.gov.tr/baglantilar/okullar/?ILKODU=${CityCode}&ILCEKODU=&SAYFANO=${PageNumber}`
+        console.log(url)
+        request(url, (err, resp, html) => {
+          if (err) return null
+          const $ = cheerio.load(html, {xmlMode: true})
+          // Satirlar alindi.
+          let td = $('td');
+          for (let j = 0; j<td.length; j++) {
+            td.each(function(i, td) {
+              var children = $(this).children();
+              if (children[0].children[0] && children[0].children[0].data) {
+                let data = children[0].children[0].data.toLowerCase();
+                let row = {
+                  city : data.split("-")[0],
+                  district: data.split("-")[1],
+                  school: data.split("-")[2]
+                }
+                /*
+                if (row.city && row.district && row.school && !similizeSchool(jsonDatas, row)) {
+                  jsonDatas.push(row);
+                }
+                */
+                console.log(row)
+                SchoolsJsonData.push(row)
               }
-            }
-          });
-        }
-
-        hasTd = td.length > 1;
-
-        pageCount++
+            });
+          }
+        })
       }
-      else {
-        console.log('Okul Yok');
-      }
-
-    } while (hasTd);
-    console.log('Şehir Bitti');
-  };
-
-
+    })
+  }
 }
 
-htmlParser();
+let writeFile = () => {
+  fs.writeFile("./turkey-meb-schools.json", JSON.stringify(SchoolsJsonData), (err) => {
+    if(err) return console.log(err)
+    console.log("Dosya Kaydedildi.")
+  })
+}
 
-fs.writeFile("./all_schools.json", JSON.stringify(doc), function(err) {
-  if(err) {
-    return console.log(err);
-  }
-
-  console.log("Dosya Kaydedildi.");
-});
-
+// https://stackoverflow.com/questions/4981891/node-js-equivalent-of-pythons-if-name-main
+if (require.main === module) {
+  async.waterfall([main, writeFile], () => {
+    console.log('done')
+  })
+}
